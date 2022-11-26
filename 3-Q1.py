e0b796342f142f0fb2c93b599e594815bb21736b
furukawaTakumi <black3moon88@gmail.com>
2022-11-26 21:16:08 +0900

    3-Q1. 頂点を塗る

--- 3-Q1.py
+++ 3-Q1.py
@@ -1,19 +1,17 @@
-from queue import Queue
-import itertools
+from collections import defaultdict
 
-n, m = map(int, input().split(" "))
+n, m = map(int, input().split())
 
 graph = [[] for _ in range(n)]
 
 for i in range(m):
-    a, b = map(int, input().split(" "))
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-destination_costs = [-1 for _ in range(n)]
+destination_costs = [-1] * n
 
-queue = Queue()
-queue.put(0)
+queue = [0]
 
 destination_costs[0] = 0
 
@@ -21,21 +19,21 @@ k = 0
 
 seen = set([0])
 
-while not queue.empty():
-    current = queue.get()
+cost_to_nodes = defaultdict(lambda: [])
+
+while len(queue) > 0:
+    current = queue.pop(0)
+
+    cost_to_nodes[destination_costs[current]].append(current)
 
     for next_vertex in graph[current]:
         if next_vertex in seen:
             continue
 
         destination_costs[next_vertex] = destination_costs[current] + 1
-        queue.put(next_vertex)
+        queue.append(next_vertex)
         seen.add(next_vertex)
 
 
-for group_num, nums in itertools.groupby(
-    sorted(enumerate(destination_costs), key=lambda x: x[1]), key=lambda x: x[1]
-):
-    if group_num == -1:
-        continue
-    print(" ".join([str(num[0]) for num in nums]))
+for nodes in cost_to_nodes.values():
+    print(" ".join([str(node) for node in sorted(nodes)]))
